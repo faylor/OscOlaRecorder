2dd22385e7453dbc20dae2cfce9d6c0b53e41bd1
faylor <32422243+faylor@users.noreply.github.com>
2021-02-05 22:14:56 +0000

    Example of how you could rewrite things. Using the ola_recorder as an object that stores state is easier. I doubt this will work immediately as I dont have the tools.

---
+++ ola_recorder.py
@@ -0,0 +1,36 @@
+import subprocess
+import enum import Enum
+
+class RunType(Enum):
+    PLAY = 2
+    RECORD = 3
+    OTHER = 4
+
+class OlaRecorder:
+
+
+    def __init__(self, name, age):
+        self.play_process = None
+        self.record_process = None
+
+    def play(self, y):
+        self.kill_all()
+        self.process = subprocess.Popen(["ola_recorder", "-p", y, "-i 0"], stdout=subprocess.PIPE)
+
+    def record(self, x):
+        self.kill_all()
+        self.process = subprocess.Popen(['ola_recorder', '-r', x, '-u 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'], stdout=subprocess.PIPE)
+
+    def kill_all(self):
+        self.kill_play()
+        self.kill_record()
+
+    def kill_play(self):
+        if self.play_process is not None:
+            self.play_process.kill()
+        self.play_process = None
+    
+    def kill_record(self):
+        if self.play_process is not None:
+            self.record_process.kill()
+        self.record_process = None
--- oscolarecorder.py
+++ oscolarecorder.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
-
-from OSC import OSCServer,OSCClient, OSCMessage, ThreadingOSCServer
+from OscOlaRecorder.ola_recorder import OlaRecorder
+from OSC import OSCServer, OSCClient, OSCMessage, ThreadingOSCServer
 import sys
 from time import sleep
 import time
@@ -13,203 +12,126 @@ from os import listdir
 from os.path import isfile, join
 from random import shuffle
 from random import sample
+from ola_recorder import OlaRecorder
 
 # TODO: add code to create "/home/pi/myoscrecs" folder if not present.
-os.chdir('/home/pi/myoscrecs')
+os.chdir("/home/pi/myoscrecs")
 time.sleep(5)
 
 # TODO: Add code to retreive ip address automatically (ethernet IP prioritized, wifi IP if ethernet not connected)
-server = ThreadingOSCServer( ("10.1.1.143", 7002) )# the ip adress of your pi/device goes here and the incoming OSC port you would like to use, i used 7002 for the port - the incoming port should be matched on your TouchOSC / OSC app
+server = ThreadingOSCServer(
+    ("10.1.1.143", 7002)
+)  # the ip adress of your pi/device goes here and the incoming OSC port you would like to use, i used 7002 for the port - the incoming port should be matched on your TouchOSC / OSC app
 client = OSCClient()
 
-#def handle_timeout(self):
-#	print ("I'm IDLE")
-#This here is just to do something while the script recieves no information....
-#server.handle_timeout = types.MethodType(handle_timeout, server)
+# def handle_timeout(self):
+# 	print ("I'm IDLE")
+# This here is just to do something while the script recieves no information....
+# server.handle_timeout = types.MethodType(handle_timeout, server)
 
-# Record Global Var
-r = None
-# Playback Global Var
-p = None
-# Stop Recording Global Var
-sr = 0
-#Stop Playback Global Var
-sp = 0
-#Set to loop Global Var
+# Set to loop Global Var
 loop = 1
-#Duration Time Global Var
+# Duration Time Global Var
 loopd = 10
-#Set to Shuffle Global Var
+# Set to Shuffle Global Var
 shuffleloop = 1
 
-#Function to record DMX input using ola_recorder program and save into banks numbered 1-XX - Recording starts and stops when triggered via OSC.
-def record(path, tags, args, source):
-		split = path.split("/1/toggle")
-		x = split.pop()
-		state=int(args[0])
-		print "Record",x
-		global sr
-		global r
-		global sp
-		global p
-		if state == 1:
-		  if sr == 0:
-		 	 if sp == 0:
-				r = subprocess.Popen(['ola_recorder', '-r', x, '-u 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'], stdout=subprocess.PIPE)
-			 	sr = 1
-		   	 else:
-				p.kill()
-				r = subprocess.Popen(['ola_recorder', '-r', x, '-u 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'], stdout=subprocess.PIPE)
-                                sr = 1
-				sp = 0
-		  else:
-		         if sp == 0:
-				r.kill()
-                         	r = subprocess.Popen(['ola_recorder', '-r', x, '-u 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'], stdout=subprocess.PIPE)
-			 	sr = 1
-			 else:
-				p.kill()
-                                r = subprocess.Popen(['ola_recorder', '-r', x, '-u 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20'], stdout=subprocess.PIPE)
-                                sr = 1
-				sp = 0
+ola = OlaRecorder()
 
-		else:
-			if sr == 0:
-				None
-			else: 
-				r.kill()
-				sr = 0
+# Function to record DMX input using ola_recorder program and save into banks numbered 1-XX
+# Recording starts and stops when triggered via OSC.
+def record(self, path, tags, args, source):
+    split = path.split("/1/toggle")
+    x = split.pop()
+    state = int(args[0])
+    print ("Record", x)
+    if state == 1:
+        ola.record(x)
+    else:
+        ola.kill_record()
 
-				
-#Function to Playback recorded DMX clips using ola_recorder program -  Playback starts and stops when triggered via OSC.
-def playback(path, tags, args, source):
-        split = path.split("/2/push")
-        y = split.pop()
-	state=int(args[0])
-        print "Playback:",y
-        global sp
-	global p
-	global sr
-	global r
-	if state == 1:
-               		if sp == 0:
-				if sr == 0:
-					p = subprocess.Popen(['ola_recorder', '-p', y, '-i 0'], stdout=subprocess.PIPE)
-					sp = 1
-				else:
-					r.kill()
-					p = subprocess.Popen(['ola_recorder', '-p', y, '-i 0'], stdout=subprocess.PIPE)
-                                        sp = 1
-					sr = 0
-			else:
-				if sr == 0:
-					p.kill()
-					p = subprocess.Popen(['ola_recorder', '-p', y, '-i 0'], stdout=subprocess.PIPE)
-					sp = 1
-				else:
-					p.kill()
-					r.kill()
-					p = subprocess.Popen(['ola_recorder', '-p', y, '-i 0'], stdout=subprocess.PIPE)
-                                        sp = 1
-					sr = 0
 
-					# Function to kill all playback
+# Function to Playback recorded DMX clips using ola_recorder program -  Playback starts and stops when triggered via OSC.
+def playback(path, tags, args, source, ola):
+    split = path.split("/2/push")
+    y = split.pop()
+    state = int(args[0])
+    print ("Playback:", y)
+    if state == 1:
+        ola.play(y)
+
+# Function to kill all playback
 def stopallplay(path, tags, args, source):
-		state=int(args[0])
-		print "Stop All Playback:", state;
-		global sp
-		global p
-        	if state == 1:
-			p.kill()
-			sp = 0
-			
-#Function to kill all recording
+    state = int(args[0])
+    print ("Stop All Playback:", state)
+    if state == 1:
+        ola.kill_play()
+
+
+# Function to kill all recording
 def stopallrec(path, tags, args, source):
-        state=int(args[0])
-        print "Stop All Recording:", state;
-        global sr
-        global r
-        if state == 1:
-                r.kill()
-                sr = 0
-		
-#Function to set loop duration 
+    state = int(args[0])
+    print ("Stop All Recording:", state)
+    if state == 1:
+        ola.kill_record()
+
+# Function to set loop duration
 def loopduration(path, tags, args, source):
-		global loopd
-		loopd=int(args[0])
-#		print "Loop Duration", loopd;
+    global loopd
+    loopd = int(args[0])
 
-#Function to play all recorded clips in sequence
+# 		print "Loop Duration", loopd;
+
+# Function to play all recorded clips in sequence
 def playall(path, tags, args, source):
-		global loop
-		state = int(args[0])
-		loop=state
-		if loop == 1:
-			playallloop()
-			
-#Function to shuffle order of clips playing back 
+    global loop
+    state = int(args[0])
+    loop = state
+    if loop == 1:
+        playallloop()
+
+
+# Function to shuffle order of clips playing back
 def shuffler(path, tags, args, source):
-                global shuffleloop
-                state = int(args[0])
-                shuffleloop=state
-                #if shuffle == 1:
+    global shuffleloop
+    state = int(args[0])
+    shuffleloop = state
+    # if shuffle == 1:
 
-#Function to loop single file
+
+# Function to loop single file
 def playallloop():
-		global sp
-		global p
-		global sr
-		global r
-		global loop
-		global loopd
-		global shuffleloop
-		while loop == 1:
-		  if loop == 0:
-			p.kill()
-			break
-		  print "Play on Loop";
-		  file_list = os.listdir(r"/home/pi/myoscrecs")
-		  #file_list_sorted = sorted([file_list, key=int)
-	          file_shuffle = sample(file_list, len(file_list))
-		  if shuffleloop == 0:
-		   	files = sorted(file_list)
-		  else:
-			files = file_shuffle
-		  for i in files:
-		     if loop == 0:
-			p.kill()
-			break
-	             #i = ""
-		     #i = str(x)
-		     print "Loop Duration", loopd;
-		     if sp == 0:
-                        if sr == 0:
-                            	p = subprocess.Popen(['ola_recorder', '-p', i, '-i 0'], stdout=subprocess.PIPE)
-                            	sp = 1
-			else:
-				r.kill()
-				p = subprocess.Popen(['ola_recorder', '-p', i, '-i 0'], stdout=subprocess.PIPE)
-				sp = 1
-				sr = 0
-                     else:
-			if sr == 0:
-				p.kill()
-				p = subprocess.Popen(['ola_recorder', '-p', i, '-i 0'], stdout=subprocess.PIPE)
-				sp = 1
-			else:
-				p.kill()
-				r.kill()
-				p = subprocess.Popen(['ola_recorder', '-p', i, '-i 0'], stdout=subprocess.PIPE)
-				sp = 1
-				sr = 0
-			time.sleep(loopd)
+    global loop
+    global loopd
+    global shuffleloop
+    while loop == 1:
+        if loop == 0:
+            ola.kill_play()
+            break
+        print ("Play on Loop")
+        file_list = os.listdir(r"/home/pi/myoscrecs")
+        # file_list_sorted = sorted([file_list, key=int)
+        file_shuffle = sample(file_list, len(file_list))
+        if shuffleloop == 0:
+            files = sorted(file_list)
+        else:
+            files = file_shuffle
+            for i in files:
+                if loop == 0:
+                    ola.kill_play()
+                    break
+                print ("Loop Duration", loopd)
+                ola.play(i)
+                time.sleep(loopd)
 
-#OSC Client message handling -You can increase the amount of record and playback banks by increasing the number from 70 to whatever in the below lines.
-for x in range(1,70):
-	        server.addMsgHandler("/1/"+"toggle"+`x`, record)
 
-for y in range(1,70):
-                server.addMsgHandler("/2/"+"push"+`y`, playback)
+# OSC Client message handling -You can increase the amount of record and playback banks by increasing the number 
+# from 70 to whatever in the below lines.
+for x in range(1, 70):
+    server.addMsgHandler("/1/" + "toggle" + repr(x), record)
+
+for y in range(1, 70):
+    server.addMsgHandler("/2/" + "push" + repr(y), playback)
 
 server.addMsgHandler("/3/stopallplay", stopallplay)
 server.addMsgHandler("/3/stopallrec", stopallrec)
@@ -218,13 +140,11 @@ server.addMsgHandler("/3/loopduration", loopduration)
 server.addMsgHandler("/3/shuffle", shuffler)
 
 
-#The way that the MSG Handlers work is by taking the values from set accessory, then it puts them into a function
-#The function then takes the values and separates them according to their class (args, source, path, and tags)
+# The way that the MSG Handlers work is by taking the values from set accessory, then it puts them into a function
+# The function then takes the values and separates them according to their class (args, source, path, and tags)
 
 while True:
-	server.handle_request()
+    server.handle_request()
 
 server.close()
-#This will kill the server when the program ends
-
-
+# This will kill the server when the program ends
